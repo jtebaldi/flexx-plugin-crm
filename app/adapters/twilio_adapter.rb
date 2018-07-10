6410dfed3f71b10e7fce51808001aeaa17297c35
Paulo Renato Castro <leitedecastro@gmail.com>
2018-07-11 00:51:52 +1000

    Rename Twilio env variables

--- app/adapters/twilio_adapter.rb
+++ app/adapters/twilio_adapter.rb
@@ -1,16 +1,16 @@
 class TwilioAdapter
   def send_sms(to:, body:)
     sms = client.api.account.messages.create(
-      from: ENV["TWILIO_NUMBER"],
+      from: ENV["TWILIO_CAMPAIGNS_NUMBER"],
       to: to,
       body: body,
-      status_callback: ENV["TWILIO_STATUS_CALLBACK"]
+      status_callback: ENV["TWILIO_CAMPAIGNS_CALLBACK"]
     )
   end
 
   def call_confirmation_flow(to:, parameters:)
     flow = client.studio.flows(ENV["TWILIO_CONFIRMATION_FLOW_SID"]).engagements.create(
-      from: ENV["TWILIO_NUMBER"],
+      from: ENV["TWILIO_CONFIRMATION_FLOW_NUMBER"],
       to: to,
       parameters: parameters.to_json
     )
