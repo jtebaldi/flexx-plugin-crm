4031b35222e2fb42441e5f0662c3217a29045750
Paulo Renato Castro <leitedecastro@gmail.com>
2018-07-19 22:56:48 +1000

    Implement tag ownership

--- config/initializers/custom_models.rb
+++ config/initializers/custom_models.rb
@@ -3,6 +3,8 @@ Rails.application.config.to_prepare do
     include Plugins::FlexxPluginCrm::Concerns::HasContacts
     include Plugins::FlexxPluginCrm::Concerns::HasTasks
 
+    acts_as_tagger
+
     has_many :task_recipes, class_name: 'Plugins::FlexxPluginCrm::TaskRecipe'
     has_many :automated_campaigns, class_name: 'Plugins::FlexxPluginCrm::AutomatedCampaign'
     has_many :automated_campaign_jobs, class_name: 'Plugins::FlexxPluginCrm::AutomatedCampaignJob'
