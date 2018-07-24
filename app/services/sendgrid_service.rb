bd6de9f89f9e89ac8675e881aa99cd50985fc5cd
Paulo Renato Castro <leitedecastro@gmail.com>
2018-07-25 01:40:46 +1000

    Add campaign to Sendgrid service

--- app/services/sendgrid_service.rb
+++ app/services/sendgrid_service.rb
@@ -19,6 +19,31 @@ class SendgridService
     JSON.parse(response.body)['persisted_recipients'][0] if (200..299).include?(response.status_code.to_i)
   end
 
+  def create_campaign(subject:, sender:, list:, body:)
+    campaign_details = {
+      subject: subject,
+      sender_id: sender,
+      list_ids: list,
+      plain_content: body
+    }
+
+    response = sg.client.campaigns.post(request_body: campaign_details)
+
+    JSON.parse(response.body)["id"] if (200..299).include?(response.status_code.to_i)
+  end
+
+  def send_campaign(campaign_id:)
+    response = sg.client.campaigns._(campaign_id).schedules.now.post()
+
+    JSON.parse(response.body)["status"] if (200.299).include?(response.status_code.to_i)
+  end
+
+  def schedule_campaign(campaign_id:, send_at:)
+    response = sg.client.campaigns._(campaign_id).schedules.post(request_body: { send_at: send_at })
+
+    JSON.parse(response.body)["status"] if (200.299).include?(response.status_code.to_i)
+  end
+
   def send_email(to:, body:, send_at:)
     params = {
       personalizations: [{
