4c07cb1d5f6174365f64ff39c48f6c27ed667047
Paulo Castro <phlcastro@gmail.com>
2018-07-26 14:55:19 +1000

    bug fix has_users concern

--- app/models/plugins/flexx_plugin_crm/concerns/has_users.rb
+++ app/models/plugins/flexx_plugin_crm/concerns/has_users.rb
@@ -1,13 +1,19 @@
 module Plugins::FlexxPluginCrm::Concerns::HasUsers
   extend ActiveSupport::Concern
 
-  module ClassMethods
+  class_methods do
     def user_class_name
       PluginRoutes.static_system_info['user_model'].presence || 'CamaleonCms::User'
     end
 
     def user_class
       user_class_name.constantize
+    end
+  end
+
+  included do
+    def user_class
+      self.class.user_class
     end
   end
 end
