46506502f3541e1b01b8d964cd6c2fb1808b3ee9
Paulo Castro <phlcastro@gmail.com>
2018-10-27 00:22:05 +1000

    duplicated message bug fix. sidekiq is faster than aasm

--- app/models/plugins/flexx_plugin_crm/concerns/send_message.rb
+++ app/models/plugins/flexx_plugin_crm/concerns/send_message.rb
@@ -10,18 +10,19 @@ module Plugins::FlexxPluginCrm::Concerns::SendMessage
       state :scheduled, initial: true
       state :draft, :sending, :sent, :received
 
-      event :send_message do
+      event :send_message, after_commit: :run_worker do
         transitions from: :scheduled, to: :sending
-
-        after do
-          SendMessageWorker.perform_async self.id if self.class == Plugins::FlexxPluginCrm::Message
-          SendEmailWorker.perform_async   self.id if self.class == Plugins::FlexxPluginCrm::Email
-        end
       end
 
       event :done do
         transitions from: :sending, to: :sent
       end
     end
+  end
+
+  def run_worker
+    byebug
+    SendMessageWorker.perform_async self.id if self.class == Plugins::FlexxPluginCrm::Message
+    SendEmailWorker.perform_async   self.id if self.class == Plugins::FlexxPluginCrm::Email
   end
 end
