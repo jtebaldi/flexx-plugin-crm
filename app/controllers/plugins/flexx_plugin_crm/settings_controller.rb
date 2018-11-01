2068fa3ef6a3e174946def3ac16d356a717dabaa
Andrei Kislichenko <android.2net@gmail.com>
2018-11-01 17:50:50 +0100

    add business email to from field dropdown. fix sender and recipient name

--- app/controllers/plugins/flexx_plugin_crm/settings_controller.rb
+++ app/controllers/plugins/flexx_plugin_crm/settings_controller.rb
@@ -1,12 +1,14 @@
 module Plugins::FlexxPluginCrm
   class SettingsController < CamaleonCms::Apps::PluginsAdminController
     layout 'flexx_next_admin'
+    before_action :set_business_email
 
     def index; end
 
     def update
       r = { user: current_user }; hooks_run('user_update', r)
       if current_user.update(user_params)
+        @business_email.update value: params[:business_email]
         current_user.set_metas(params[:meta]) if params[:meta].present?
         current_user.set_field_values(params[:field_options])
         r = { user: current_user, message: t('camaleon_cms.admin.users.message.updated'), params: params }; hooks_run('user_after_edited', r)
@@ -23,6 +25,10 @@ module Plugins::FlexxPluginCrm
     end
 
     private
+
+    def set_business_email
+      @business_email = current_site.custom_field_values.find_by_custom_field_slug 'business_email'
+    end
 
     def user_params
       parameters = params.require(:user)
