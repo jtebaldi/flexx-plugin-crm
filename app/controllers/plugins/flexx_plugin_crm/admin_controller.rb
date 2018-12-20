ac0d0b4f82c83ad6357ac52057c7c5caffb00b72
Andrei Kislichenko <android.2net@gmail.com>
2018-12-20 22:59:01 +0100

    Hide archived contacts in new task creation

--- app/controllers/plugins/flexx_plugin_crm/admin_controller.rb
+++ app/controllers/plugins/flexx_plugin_crm/admin_controller.rb
@@ -118,7 +118,7 @@ module Plugins::FlexxPluginCrm
     end
 
     def list_contacts_with_email
-      render json: current_site.contacts.map { |c| { name: "#{c.print_name} - #{c.email}", value: c.id } }
+      render json: current_site.contacts.active.map { |c| { name: "#{c.print_name} - #{c.email}", value: c.id } }
     end
 
     def from_form
