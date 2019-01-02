5f1615deed259cc849d8acd54c1e7ffceef05448
Paulo Castro <phlcastro@gmail.com>
2019-01-02 19:29:42 +1000

    new sms_blasts model

--- app/models/plugins/flexx_plugin_crm/email.rb
+++ app/models/plugins/flexx_plugin_crm/email.rb
@@ -4,5 +4,11 @@ class Plugins::FlexxPluginCrm::Email < ActiveRecord::Base
   self.table_name = 'emails'
 
   belongs_to :site, class_name: 'CamaleonCms::Site'
+
   has_many :email_recipients, class_name: 'Plugins::FlexxPluginCrm::EmailRecipient', dependent: :destroy
+
+  scope :draft, -> { where(aasm_state: 'draft') }
+  scope :recent, -> { where(aasm_state: ['sent', 'scheduled']).order(send_at: :desc) }
+  scope :scheduled, -> { where(aasm_state: 'scheduled') }
+  scope :sent, -> { where(aasm_state: 'sent') }
 end
