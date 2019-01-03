5a302c31c02e7bf2aaacaf0b7ad46ee24876ae83
Paulo Castro <phlcastro@gmail.com>
2019-01-03 22:33:42 +1000

    simpliflying SendMessage concern aasm states

--- app/models/plugins/flexx_plugin_crm/concerns/send_message.rb
+++ app/models/plugins/flexx_plugin_crm/concerns/send_message.rb
@@ -8,22 +8,14 @@ module Plugins::FlexxPluginCrm::Concerns::SendMessage
 
     aasm do
       state :scheduled, initial: true
-      state :draft, :sending, :task_scheduled, :task_sending, :sent, :task_sent, :received
+      state :draft, :sending, :sent, :received
 
       event :send_message, after_commit: :run_worker do
         transitions from: :scheduled, to: :sending
       end
 
-      event :send_task_message, after_commit: :run_worker do
-        transitions from: :task_scheduled, to: :task_sending
-      end
-
       event :done do
         transitions from: :sending, to: :sent
-      end
-
-      event :task_done do
-        transitions from: :task_sending, to: :task_sent
       end
     end
   end
