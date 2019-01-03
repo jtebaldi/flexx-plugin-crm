e51313945cd2b25a5b2620cc58b14b56a7d39ed4
Paulo Castro <phlcastro@gmail.com>
2019-01-03 22:38:26 +1000

    included message blasts

--- app/services/messages_job_service.rb
+++ app/services/messages_job_service.rb
@@ -4,83 +4,74 @@ require 'twilio-ruby'
 class MessagesJobService
   extend SendGrid
 
-  class << self
-
-    def queue_new_messages
-      Plugins::FlexxPluginCrm::Message.scheduled.where('send_at <= ?', Time.current).each do |msg|
-        msg.send_message!
-      end
-
-      Plugins::FlexxPluginCrm::Message.task_scheduled.where('send_at <= ?', Time.current).each do |msg|
-        msg.send_task_message!
-      end
-
-      Plugins::FlexxPluginCrm::Email.scheduled.where('send_at <= ?', Time.current).each do |email|
-        email.send_message!
-      end
-
-      Plugins::FlexxPluginCrm::Email.task_scheduled.where('send_at <= ?', Time.current).each do |email|
-        email.send_task_message!
-      end
+  def self.queue_new_messages
+    Plugins::FlexxPluginCrm::Message.scheduled.where('send_at <= ?', Time.current).each do |message|
+      message.send_message!
     end
 
-    # Send email message.
-    # @param msg [Email] email to send
-    # @return [TrueClass, FalseClass] true if the message was sent successfully
-    def send_email(email)
-      sender = CamaleonCms::User.find_by_email email.from
-      if sender
-        from = { email: email.from, name: sender.print_name }
-      else
-        be = email.site.custom_field_values.find_by_custom_field_slug 'business_email'
-        from = { email: be.value, name: email.site.name }
-      end
-      sg_message_id = SendgridAdapter.new(site: email.site).send_email(
-        from: from,
-        to: email.email_recipients.map { |r| { email: r.to, name: r.contact.print_name } },
-        subject: email.subject,
-        body: email.body
-      )
-
-      if email.sending? # email blast
-        email.email_recipients.includes(:contact).each do |r|
-          r.create_task(
-            contact: r.contact,
-            site: email.site,
-            aasm_state: :done,
-            created_by: email.created_by,
-            details: 'Email blast',
-            task_type: :email,
-            title: 'Email blast',
-            updated_by: email.created_by
-          )
-        end
-        email.update(sg_message_id: sg_message_id) && email.done!
-      else
-        email.update(sg_message_id: sg_message_id) && email.task_done!
-      end
+    Plugins::FlexxPluginCrm::MessageBlast.scheduled.where('send_at <= ?', Time.current).each do |message_blast|
+      message_blast.send_messages!
     end
 
-    # Send sms message.
-    # @param msg [Message] nessage to send
-    # @return [TrueClass, FalseClass] true if the message was sent successfully
-    def send_sms(message)
-      result = TwilioAdapter.new.send_sms(message: message)
-      if message.sending? # sms blast
-        message.create_task(
-          site: message.site,
-          contact: message.contact,
+    Plugins::FlexxPluginCrm::Email.scheduled.where('send_at <= ?', Time.current).each do |email|
+      email.send_message!
+    end
+
+    Plugins::FlexxPluginCrm::Email.task_scheduled.where('send_at <= ?', Time.current).each do |email|
+      email.send_task_message!
+    end
+  end
+
+  def send_email(email)
+    sender = CamaleonCms::User.find_by_email email.from
+    if sender
+      from = { email: email.from, name: sender.print_name }
+    else
+      be = email.site.custom_field_values.find_by_custom_field_slug 'business_email'
+      from = { email: be.value, name: email.site.name }
+    end
+    sg_message_id = SendgridAdapter.new(site: email.site).send_email(
+      from: from,
+      to: email.email_recipients.map { |r| { email: r.to, name: r.contact.print_name } },
+      subject: email.subject,
+      body: email.body
+    )
+
+    if email.sending? # email blast
+      email.email_recipients.includes(:contact).each do |r|
+        r.create_task(
+          contact: r.contact,
+          site: email.site,
           aasm_state: :done,
-          created_by: message.created_by,
-          details: 'SMS blast',
-          task_type: :message,
-          title: 'SMS blast',
-          updated_by: message.created_by
+          created_by: email.created_by,
+          details: 'Email blast',
+          task_type: :email,
+          title: 'Email blast',
+          updated_by: email.created_by
         )
-        result.error_code.zero? && message.done! && message.update(sid: result.sid)
-      else
-        result.error_code.zero? && message.task_done! && message.update(sid: result.sid)
       end
+      email.update(sg_message_id: sg_message_id) && email.done!
+    else
+      email.update(sg_message_id: sg_message_id) && email.task_done!
+    end
+  end
+
+  def send_sms(message)
+    result = TwilioAdapter.new.send_sms(message: message)
+    if message.sending? # sms blast
+      message.create_task(
+        site: message.site,
+        contact: message.contact,
+        aasm_state: :done,
+        created_by: message.created_by,
+        details: 'SMS blast',
+        task_type: :message,
+        title: 'SMS blast',
+        updated_by: message.created_by
+      )
+      result.error_code.zero? && message.done! && message.update(sid: result.sid)
+    else
+      result.error_code.zero? && message.task_done! && message.update(sid: result.sid)
     end
   end
 end
