3e8452c6457c58377df83bdd33e2c013d16732e5
Paulo Castro <phlcastro@gmail.com>
2019-01-04 22:27:49 +1000

    parse message blast body before start to send it to contacts

--- app/services/message_blast_service.rb
+++ app/services/message_blast_service.rb
@@ -20,7 +20,7 @@ class MessageBlastService
         contact_id: contact.id,
         from_number: @site.get_option('twilio_campaigns_number'),
         to_number: EngageToolsService.add_country_code(contact.phonenumbers.mobile.first.number),
-        message: DynamicFieldsParserService.parse_contact(site: @site, template: @body, contact: contact, escape: false),
+        message: DynamicFieldsParserService.parse_contact(site: @site, template: @body, contact: contact),
         aasm_state: (task || task.nil? ? :task_scheduled : :scheduled),
         send_at: send_at,
         created_by: @user.id
