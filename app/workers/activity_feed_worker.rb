33732a9d43e315de113a5510c3cb8c5c6cc5a2bc
Paulo Castro <phlcastro@gmail.com>
2019-01-29 23:42:17 +1000

    subscribing to real time notifications feed

--- app/workers/activity_feed_worker.rb
+++ app/workers/activity_feed_worker.rb
@@ -1,5 +1,4 @@
 require 'sidekiq'
-require 'stream'
 
 class ActivityFeedWorker
   include Sidekiq::Worker
@@ -7,21 +6,13 @@ class ActivityFeedWorker
   def perform(args)
     params = JSON.parse(args, symbolize_names: true)
 
-    stream_client.feed(
+    StreamAdapter.client.feed(
       params[:feed_name],
       params[:feed_id]
     ).add_activity(
       actor: params[:actor],
       verb: params[:verb],
       object: params[:object]
-    )
-  end
-
-  def stream_client
-    @stream_client ||= Stream::Client.new(
-      ENV['STREAM_API_KEY'],
-      ENV['STREAM_API_SECRET'],
-      location: ENV['STREAM_APP_LOCATION']
     )
   end
 end
