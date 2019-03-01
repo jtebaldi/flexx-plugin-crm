68b004659c0e84e2460b5d34855941e5d5dc53d2
Paulo Castro <phlcastro@gmail.com>
2019-03-01 22:30:03 +1000

    listing available forms

--- app/controllers/plugins/flexx_plugin_crm/forms_controller.rb
+++ app/controllers/plugins/flexx_plugin_crm/forms_controller.rb
@@ -2,10 +2,10 @@ module Plugins::FlexxPluginCrm
   class FormsController < Plugins::FlexxPluginCrm::ApplicationController
     layout "layouts/flexx_next_admin"
 
-    def index; end
+    def index
+      @forms = current_site.contact_forms.where(parent_id: nil).order(:name)
+    end
 
     def edit; end
-
-    def view; end
   end
 end
