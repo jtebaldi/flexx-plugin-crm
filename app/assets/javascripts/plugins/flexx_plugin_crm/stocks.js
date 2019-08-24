649df0d034d2883a2ddf1eeae81553a18a29a316
Joe Tebaldi <joetebaldi@gmail.com>
2019-08-24 02:20:38 -0400

    stock fixes to allowable characters, css and validation

--- app/assets/javascripts/plugins/flexx_plugin_crm/stocks.js
+++ app/assets/javascripts/plugins/flexx_plugin_crm/stocks.js
@@ -39,10 +39,15 @@ function insertDynamicField(textareaInput, mergeField) {
     $(textareaInput).val(textBefore + mergeField + textAfter);
 }
 
-function validateStockLabel(e) {
-  console.log(e.key)
-  if (e.key != 'Shift' && e.key != 'Tab' && e.key.match(/[^a-z_\-]/gi))
+function validateStockLabel(e) {  
+  var stocklabel = $('#stockLabel');
+  
+  if (e.key == ' ') {    
+    stocklabel.val(stocklabel.val() + "_");
     e.preventDefault();
+  } else if (e.key != 'Shift' && e.key != 'Tab' && e.key.match(/[^a-z0-9_\-]/gi)) {
+    e.preventDefault();
+  }
 }
 
 app.ready(function() {
@@ -64,20 +69,20 @@ app.ready(function() {
       custom: {
         'snippet-code': ($elm) => {
           if (['contact_email', 'contact_first_name', 'contact_last_name'].includes($elm.val())) {
-            return 'The code is reserver for system snippet.';
+            return 'The code is reserved for system snippet.';
           }
         },
         'snippet-name': ($elm) => {
           if (['E-mail', 'First Name', 'Last Name'].includes($elm.val())) {
             return 'The name is reserved for system snippet.';
-          }
+          }          
         }
       }
     }).on('submit', (e) => {
       if (e.isDefaultPrevented()) {
         e.stopPropagation();
       } else {
-
+        
       }
     });
   });
