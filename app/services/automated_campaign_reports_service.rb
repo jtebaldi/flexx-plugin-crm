683b9434c5f54242c483941f01a380987009330a
Paulo Castro <phlcastro@gmail.com>
2019-11-17 17:19:39 +1000

    ordered steps

--- app/services/automated_campaign_reports_service.rb
+++ app/services/automated_campaign_reports_service.rb
@@ -27,11 +27,11 @@ class AutomatedCampaignReportsService
       steps: []
     }
 
-    @campaign.steps.each do |step|
+    @campaign.ordered_steps.each do |step|
       emails = Plugins::FlexxPluginCrm::Email.all.where(automated_campaign_subscription_step_id: step.subscription_steps.pluck(:id))
       step_result = {
         name: step.name,
-        contacts: @campaign.subscriptions.not_ended.where(next_step: step.id).count,
+        contacts: @campaign.subscriptions.not_ended.where(next_step: step.subscription_steps.pluck(:id)).count,
         emails_sent: emails.sent.count,
         emails_opened: emails.sum(:opened_count),
         emails_clicked: emails.sum(:clicked_count),
